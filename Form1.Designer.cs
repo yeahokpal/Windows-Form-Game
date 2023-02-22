ada4e084ea37e2ff9bef7f0f8619fe0b4f6839fa
yeahokpal <113112958+yeahokpal@users.noreply.github.com>
2023-02-22 08:32:53 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿
+namespace demonstrationProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pBox1 = new System.Windows.Forms.PictureBox();
+            this.pBox2 = new System.Windows.Forms.PictureBox();
+            this.pBox3 = new System.Windows.Forms.PictureBox();
+            this.pBox4 = new System.Windows.Forms.PictureBox();
+            this.pBox5 = new System.Windows.Forms.PictureBox();
+            this.pBox6 = new System.Windows.Forms.PictureBox();
+            this.pBox7 = new System.Windows.Forms.PictureBox();
+            this.pBox8 = new System.Windows.Forms.PictureBox();
+            this.pBox9 = new System.Windows.Forms.PictureBox();
+            this.cmbDifficultySelect = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(29, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(129, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(229, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(29, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(129, 79);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(229, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(29, 129);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(129, 129);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(229, 129);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(129, 178);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pBox1
+            // 
+            this.pBox1.Image = ((System.Drawing.Image)(resources.GetObject("pBox1.Image")));
+            this.pBox1.Location = new System.Drawing.Point(29, 29);
+            this.pBox1.Name = "pBox1";
+            this.pBox1.Size = new System.Drawing.Size(75, 23);
+            this.pBox1.TabIndex = 10;
+            this.pBox1.TabStop = false;
+            this.pBox1.Visible = false;
+            // 
+            // pBox2
+            // 
+            this.pBox2.Image = ((System.Drawing.Image)(resources.GetObject("pBox2.Image")));
+            this.pBox2.Location = new System.Drawing.Point(129, 29);
+            this.pBox2.Name = "pBox2";
+            this.pBox2.Size = new System.Drawing.Size(75, 23);
+            this.pBox2.TabIndex = 11;
+            this.pBox2.TabStop = false;
+            this.pBox2.Visible = false;
+            // 
+            // pBox3
+            // 
+            this.pBox3.Image = ((System.Drawing.Image)(resources.GetObject("pBox3.Image")));
+            this.pBox3.Location = new System.Drawing.Point(229, 29);
+            this.pBox3.Name = "pBox3";
+            this.pBox3.Size = new System.Drawing.Size(75, 23);
+            this.pBox3.TabIndex = 12;
+            this.pBox3.TabStop = false;
+            this.pBox3.Visible = false;
+            // 
+            // pBox4
+            // 
+            this.pBox4.Image = ((System.Drawing.Image)(resources.GetObject("pBox4.Image")));
+            this.pBox4.Location = new System.Drawing.Point(29, 79);
+            this.pBox4.Name = "pBox4";
+            this.pBox4.Size = new System.Drawing.Size(75, 23);
+            this.pBox4.TabIndex = 13;
+            this.pBox4.TabStop = false;
+            this.pBox4.Visible = false;
+            // 
+            // pBox5
+            // 
+            this.pBox5.Image = ((System.Drawing.Image)(resources.GetObject("pBox5.Image")));
+            this.pBox5.Location = new System.Drawing.Point(129, 79);
+            this.pBox5.Name = "pBox5";
+            this.pBox5.Size = new System.Drawing.Size(75, 23);
+            this.pBox5.TabIndex = 14;
+            this.pBox5.TabStop = false;
+            this.pBox5.Visible = false;
+            // 
+            // pBox6
+            // 
+            this.pBox6.Image = ((System.Drawing.Image)(resources.GetObject("pBox6.Image")));
+            this.pBox6.Location = new System.Drawing.Point(229, 79);
+            this.pBox6.Name = "pBox6";
+            this.pBox6.Size = new System.Drawing.Size(75, 23);
+            this.pBox6.TabIndex = 15;
+            this.pBox6.TabStop = false;
+            this.pBox6.Visible = false;
+            // 
+            // pBox7
+            // 
+            this.pBox7.Image = ((System.Drawing.Image)(resources.GetObject("pBox7.Image")));
+            this.pBox7.Location = new System.Drawing.Point(29, 129);
+            this.pBox7.Name = "pBox7";
+            this.pBox7.Size = new System.Drawing.Size(75, 23);
+            this.pBox7.TabIndex = 16;
+            this.pBox7.TabStop = false;
+            this.pBox7.Visible = false;
+            // 
+            // pBox8
+            // 
+            this.pBox8.Image = ((System.Drawing.Image)(resources.GetObject("pBox8.Image")));
+            this.pBox8.Location = new System.Drawing.Point(129, 129);
+            this.pBox8.Name = "pBox8";
+            this.pBox8.Size = new System.Drawing.Size(75, 23);
+            this.pBox8.TabIndex = 17;
+            this.pBox8.TabStop = false;
+            this.pBox8.Visible = false;
+            // 
+            // pBox9
+            // 
+            this.pBox9.Image = ((System.Drawing.Image)(resources.GetObject("pBox9.Image")));
+            this.pBox9.Location = new System.Drawing.Point(229, 129);
+            this.pBox9.Name = "pBox9";
+            this.pBox9.Size = new System.Drawing.Size(75, 23);
+            this.pBox9.TabIndex = 18;
+            this.pBox9.TabStop = false;
+            this.pBox9.Visible = false;
+            // 
+            // cmbDifficultySelect
+            // 
+            this.cmbDifficultySelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDifficultySelect.FormattingEnabled = true;
+            this.cmbDifficultySelect.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "100"});
+            this.cmbDifficultySelect.Location = new System.Drawing.Point(107, 238);
+            this.cmbDifficultySelect.Name = "cmbDifficultySelect";
+            this.cmbDifficultySelect.Size = new System.Drawing.Size(121, 21);
+            this.cmbDifficultySelect.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(143, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Difficulty:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(334, 271);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbDifficultySelect);
+            this.Controls.Add(this.pBox9);
+            this.Controls.Add(this.pBox8);
+            this.Controls.Add(this.pBox7);
+            this.Controls.Add(this.pBox6);
+            this.Controls.Add(this.pBox5);
+            this.Controls.Add(this.pBox4);
+            this.Controls.Add(this.pBox3);
+            this.Controls.Add(this.pBox2);
+            this.Controls.Add(this.pBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pBox1;
+        private System.Windows.Forms.PictureBox pBox2;
+        private System.Windows.Forms.PictureBox pBox3;
+        private System.Windows.Forms.PictureBox pBox4;
+        private System.Windows.Forms.PictureBox pBox5;
+        private System.Windows.Forms.PictureBox pBox6;
+        private System.Windows.Forms.PictureBox pBox7;
+        private System.Windows.Forms.PictureBox pBox8;
+        private System.Windows.Forms.PictureBox pBox9;
+        private System.Windows.Forms.ComboBox cmbDifficultySelect;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace demonstrationProject
+{
+    public partial class Form1 : Form
+    {
+        /*Purpose: create a memorization game that gives random numbers which the user must input in the form of buttons
+         * Input: Button presses
+         * Output: Win or Loss
+         */
+
+        //creating variables and timer
+        string correctAnswer, userInput, pBoxName;
+        int btnPresses, count;
+        System.Windows.Forms.Timer timer1;
+
+        //creates sound player
+        SoundPlayer funneeeBoom = new SoundPlayer("vine-boom.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void onButtonPress()//happens every time a game button is pressed after start, adds to a counter and if 5 buttons are pressed, ends and resets game
+        {
+            btnPresses += 1;
+            if (btnPresses >= Convert.ToInt16(cmbDifficultySelect.SelectedItem))//occurs when the user presses number of buttons determined by difficulty
+            {
+                timer1.Dispose();//deletes timer after game is played to reduce memory usage
+
+                //disables all game buttons, enables the start button and difficulty select for next attempt
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+                button9.Enabled = false;
+                btnStart.Enabled = true;
+                cmbDifficultySelect.Enabled = true;
+
+                if (userInput == correctAnswer)//shows message box based on whether or not the user was correct
+                {
+                    MessageBox.Show("Correct Answer");//correct answer
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect Answer");//incorrect answer
+                }
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)//user presses start
+        {
+            //resets variables
+            correctAnswer = "";
+            userInput = "";
+            pBoxName = "";
+            count = 0;
+            btnPresses = 0;
+
+            if (cmbDifficultySelect.SelectedIndex == -1)//checks for blank combo box
+            {
+                MessageBox.Show("Please Select a Difficulty");
+            }
+            else//runs program if difficulty is selected
+            {
+                //disables start and combo box
+                btnStart.Enabled = false;
+                cmbDifficultySelect.Enabled = false;
+
+                //creates the order for that attempt
+                Random rnd = new Random();
+                for (int i = 0; i < Convert.ToInt16(cmbDifficultySelect.SelectedItem); ++i)
+                {
+                    int num = rnd.Next(1, 10);
+                    correctAnswer += num;
+                }
+
+                //enables timer to show correct buttons
+                timer1 = new System.Windows.Forms.Timer();
+                timer1.Interval = 1000;
+                timer1.Enabled = true;
+                timer1.Tick += new System.EventHandler(OnTimerEvent);
+            }
+        }
+        private void OnTimerEvent(object sender, EventArgs e)//happens every half second after start is pressed
+        {
+            if (count == correctAnswer.Length)//if all images have been shown, enable game
+            {
+                timer1.Enabled = false;
+                this.Controls[pBoxName].Visible = false;
+
+                //enables all game buttons
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                button9.Enabled = true;
+            }
+            else//continue to show pictures if not all have been shown
+            {
+                for (int i = 1; i < 10; ++i)
+                {
+                    pBoxName = "pBox" + i.ToString();
+                    this.Controls[pBoxName].Visible = false;
+                }
+
+                pBoxName = "pBox" + correctAnswer[count].ToString();
+                this.Controls[pBoxName].Visible = true;
+                funneeeBoom.Play();
+                count++;
+            }
+
+        }
+        //buttons 1-9, adds to user input string which is checked against the correct answer and adds to a variable which ends the game at 5
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userInput += "1";
+            onButtonPress();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            userInput += "2";
+            onButtonPress();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            userInput += "3";
+            onButtonPress();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            userInput += "4";
+            onButtonPress();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            userInput += "5";
+            onButtonPress();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            userInput += "6";
+            onButtonPress();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            userInput += "7";
+            onButtonPress();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            userInput += "8";
+            onButtonPress();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            userInput += "9";
+            onButtonPress();
+        }
+    }
+}
